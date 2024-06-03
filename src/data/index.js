d05e998a295835ac1f0bcfa364182246c1890fc4
guinhomoraes <guilhermeguilherme98@gmail.com>
2024-06-03 17:48:11 -0300

    Melhoria no projeto
    Deixando o projeto mais dinâmico onde o jogador pode selecionar outros jogadores

---
+++ src/data/index.js
@@ -0,0 +1,53 @@
+const players = [
+    {
+     ID: 1,
+     NOME: "Mario",
+     VELOCIDADE: 4,
+     MANOBRABILIDADE: 3,
+     PODER: 3,
+     PONTOS: 0,
+   },
+   {
+     ID: 2,
+     NOME: "Peach",
+     VELOCIDADE: 3,
+     MANOBRABILIDADE: 4,
+     PODER: 2,
+     PONTOS: 0,
+   },
+   {
+     ID: 3,
+     NOME: "Yoshi",
+     VELOCIDADE: 2,
+     MANOBRABILIDADE: 4,
+     PODER: 3,
+     PONTOS: 0,
+   },
+   {
+     ID: 4,
+     NOME: "Bowser",
+     VELOCIDADE: 5,
+     MANOBRABILIDADE: 2,
+     PODER: 5,
+     PONTOS: 0,
+   },
+   {
+     ID: 5,
+     NOME: "Luigi",
+     VELOCIDADE: 3,
+     MANOBRABILIDADE: 4,
+     PODER: 4,
+     PONTOS: 0,
+   },
+   {
+    ID: 6,
+    NOME: "Donkey Kong",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+  }
+];
+
+module.exports = players;
+
